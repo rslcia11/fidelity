6b38adc15f6ce3bd2bb95e880fef39180d833a42
Wilson Martinez <wm911m@gmail.com>
2026-02-13 11:36:22 -0500

    Remove sensitive config file

--- lib/core/config/supabase_config.dart
+++
@@ -1,4 +0,0 @@
-class SupabaseConfig {
-  static const String supabaseUrl = 'https://wdxqttifkznlswgyhywb.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkeHF0dGlma3pubHN3Z3loeXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5OTkwNTAsImV4cCI6MjA4NTU3NTA1MH0.AE8nWVgWmSumGWPqQntB6I5wAlZJuVLTBacDZcNWkdw';
-}
